dfa6fbfdcfe63afb95e41020a28fe34c17667ec8
Sasha Semenishchev <sashasemenishchev@gmail.com>
2022-07-12 19:57:29 +0200

    Added Cmake

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -336,28 +336,26 @@ std::ostream& operator<<(std::ostream& os, const Token::Kind& kind) {
 
 void Lexer::test() {
 
-        auto code =
-        "x = 2\n"
-        "// This is a comment.\n"
-        "var x\n"
-        "var y\n"
-        "var f = function(x, y) { sin(x) * sin(y) + x * y; }\n"
-        "der(f, x)\n"
-        "var g = function(x, y) { 2 * (x + der(f, y)); } // der(f, y) is a "
-        "matrix\n"
-        "var r{3}; // Vector of three elements\n"
-        "var J{12, 12}; // Matrix of 12x12 elements\n"
-        "var dot = function(u{:}, v{:}) -> scalar {\n"
-        "          return u[i] * v[i]; // Einstein notation\n"
-        "}\n"
-        "var norm = function(u{:}) -> scalar { return sqrt(dot(u, u)); }\n"
-        "var norm = function(u{:}) -> scalar { return u >= u; }\n"
-        "<end>";
+    auto code =
+    "x = 2\n"
+    "// This is a comment.\n"
+    "var x\n"
+    "var y\n"
+    "var f = function(x, y) { sin(x) * sin(y) + x * y; }\n"
+    "der(f, x)\n"
+    "var g = function(x, y) { 2 * (x + der(f, y)); } // der(f, y) is a "
+    "matrix\n"
+    "var r{3}; // Vector of three elements\n"
+    "var J{12, 12}; // Matrix of 12x12 elements\n"
+    "var dot = function(u{:}, v{:}) -> scalar {\n"
+    "          return u[i] * v[i]; // Einstein notation\n"
+    "}\n"
+    "var norm = function(u{:}) -> scalar { return sqrt(dot(u, u)); }\n"
+    "var norm = function(u{:}) -> scalar { return u >= u; }\n"
+    "<end>";
 
-        Lexer lex(code);
-        for (auto token = lex.next();
-            not token.is_one_of(Token::Kind::End, Token::Kind::Unexpected);
-            token = lex.next()) {
-            std::cout << std::setw(12) << token.kind() << " |" << token.lexeme() << "|\n";
-        }
+    Lexer lex(code);
+    for (auto token = lex.next(); !token.is_one_of(Token::Kind::End, Token::Kind::Unexpected); token = lex.next()) {
+        std::cout << std::setw(12) << token.kind() << " |" << token.lexeme() << "|\n";
+    }
 }
