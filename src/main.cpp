5b03716ace2998bda03af175144ccd2d44ed7b98
SashaSemenishchev <sashasemenishchev@gmail.com>
2022-07-16 15:58:09 +0300

    test outptuts

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include "main.h"
+#include <sys/stat.h>
 
-int main() {
+long getFileSize(std::string filename)
+{
+    struct stat stat_buf;
+    int rc = stat(filename.c_str(), &stat_buf);
+    return rc == 0 ? stat_buf.st_size : -1;
+}
+
+
+int main(int argc, char* argv[]) {
     // TODO:
     // read input file
     // tokenize and parse it
     // generate LLVM IR
     // compiler generated LLVM IR
-    Lexer::test();
+    cout << argc << " " << *argv[1] << "\n";
+
+    //Lexer::test();
 
     return 0;
 }
