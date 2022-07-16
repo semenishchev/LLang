6c74a6fdd9a815b2beb81d1e1ca6b1ecdaae6a06
Sasha Semenishchev <sashasemenishchev@gmail.com>
2022-07-16 17:21:48 +0200

    Added more AST elements

--- src/ast.h
+++ src/ast.h
@@ -4,6 +4,9 @@
 #include <memory>
 
 #include "lexer.h"
+#include "system.h"
+
+using move;
 
 namespace ast {
     class ExprAST {
@@ -11,60 +14,185 @@ namespace ast {
         virtual ~ExprAST() = default;
     };
 
-    class NumberExprAST : public ExprAST {
+    class MarkerExprAST : public ExprAST {
+    public:
+        std::string name;
+        MarkerExprAST(std::string name) : name(move(name)) {}
+    };
+
+    class MemberExprAST;
+    class TypenameAST : public ExprAST {
+    public:
+        TypenameAST(std::string name) : name(move(name)) {}
+        TypenameAST(std::string name, std::vector<ast::MemberExprAST> members) : name(move(name)), members(move(members)) {}
+        std::string name;
+        std::vector<MemberExprAST> members;
+        std::vector<MarkerExprAST> markers;
+    };
+
+    class U8ExprAST : public TypenameAST {
+    public:
+        uint8_t val;
+        U8ExprAST(uint8_t val) : TypenameAST("u8"), val(val) {}
+    };
+
+    class I16ExprAST : public TypenameAST {
+    public:
+        int val;
+        I16ExprAST(int val) : TypenameAST("i16"), val(val) {}
+    };
+
+    class U16ExprAST : public TypenameAST {
+    public:
+        uint16_t val;
+        U16ExprAST(uint16_t val) : TypenameAST("u16"), val(val) {}
+    };
+
+    class I32ExprAST : public TypenameAST {
+    public:
+        int32_t val;
+        I32ExprAST(int32_t val) : TypenameAST("i32"), val(val) {}
+    };
+
+    class U32ExprAST : public TypenameAST {
+    public:
+        uint32_t val;
+        U32ExprAST(uint32_t val) : TypenameAST("u32"), val(val) {}
+    };
+
+    class I64ExprAST : public TypenameAST {
+    public:
+        int64_t val;
+        I64ExprAST(int64_t val) : TypenameAST("i64"), val(val) {}
+    };
+
+    class U64ExprAST : public TypenameAST {
+    public:
+        uint64_t val;
+        U64ExprAST(uint64_t val) : TypenameAST("u64"), val(val) {}
+    };
+
+    class F32ExprAST : public TypenameAST {
+    public:
+        float val;
+        F32ExprAST(float val) : TypenameAST("f32"), val(val) {}
+    };
+
+    class F64ExprAST : public TypenameAST {
+    public:
         double val;
-
-    public:
-        NumberExprAST(double val) : val(val) {}
+        F64ExprAST(double val) : TypenameAST("f64"), val(val) {}
     };
 
-    class VariableExprAST : public ExprAST {
+    class BooleanExprAST : public TypenameAST {
+    public:
+        bool val;
+        BooleanExprAST(bool val) : TypenameAST("bool"), val(val) {}
+    };
+
+    class StringExprAST : public TypenameAST {
+    public:
+        types::String val;
+        StringExprAST(types::String val) : TypenameAST("string"), val(move(val)) {}
+    };
+
+    enum Visibility {
+        Public,
+        Protected,
+        Private
+    };
+
+    class MemberExprAST : public ExprAST {
+    public:
+        Visibility visibility;
+        std::vector<MarkerExprAST> markers;
+        MemberExprAST(Visibility visibility) : visibility(visibility) {}
+    };
+
+    class PrototypeAST : public MemberExprAST{
+    public:
         std::string name;
-
-    public:
-        VariableExprAST(std::string name) : name(std::move(name)) {}
-    };
-
-    class BinaryExprAST : public ExprAST {
-        char op;
-        std::unique_ptr<ExprAST> LHS, RHS;
-
-    public:
-        BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS,
-                      std::unique_ptr<ExprAST> RHS)
-                : op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
-    };
-
-/// CallExprAST - Expression class for function calls.
-    class CallExprAST : public ExprAST {
-        std::string callee;
-        std::vector<std::pair<std::string, ExprAST>> args;
-
-    public:
-        CallExprAST(std::string Callee,
-                    std::vector<std::pair<std::string, ExprAST>> args)
-                : callee(std::move(Callee)), args(std::move(args)) {}
-    };
-
-    class PrototypeAST {
-        std::string name;
-        std::vector<std::pair<std::string, std::string>> args;
-
-    public:
-        PrototypeAST(std::string name, std::vector<std::pair<std::string, std::string>> args)
-                : name(std::move(name)), args(std::move(args)) {}
+        TypenameAST return_type;
+        std::vector<std::pair<TypenameAST, std::string>> args;
+        PrototypeAST(Visibility visibility, TypenameAST return_type, std::string name, std::vector<std::pair<TypenameAST, std::string>> args)
+                : MemberExprAST(visibility), return_type(std::move(return_type)), name(move(name)), args(move(args)) {}
 
         const std::string &getName() const { return name; }
     };
 
 /// FunctionAST - This class represents a function definition itself.
-    class FunctionAST {
+
+    class MethodExprAST : public MemberExprAST {
+    public:
         std::unique_ptr<PrototypeAST> proto;
         std::unique_ptr<ExprAST> body;
+    };
 
+    class FieldExprAST : public MemberExprAST {
     public:
-        FunctionAST(std::unique_ptr<PrototypeAST> proto,
-                    std::unique_ptr<ExprAST> body)
-                : proto(std::move(proto)), body(std::move(body)) {}
+        TypenameAST type;
+        std::string name;
+        FieldExprAST(Visibility visibility, TypenameAST type, std::string name) : MemberExprAST(visibility), type(std::move(type)), name(move(name)) {}
+    };
+
+    class InterfaceExprAST : public TypenameAST {
+    public:
+        std::string name;
+        std::vector<InterfaceExprAST> implements;
+        InterfaceExprAST(std::string name, std::vector<MemberExprAST> members) : name(move(name)), TypenameAST(move(name), move(members)) {}
+        InterfaceExprAST(std::string name, std::vector<MemberExprAST> members, std::vector<InterfaceExprAST> implements)
+        : name(move(name)), TypenameAST(move(name), move(members)), implements(move(implements)) {}
+    };
+
+    class ClassExprAST : public TypenameAST {
+    public:
+        ClassExprAST* superclass;
+        std::vector<InterfaceExprAST> implements;
+        bool is_abstract = false;
+        ClassExprAST(std::string name, std::vector<MemberExprAST> members, ClassExprAST& superclass) : TypenameAST(move(name), move(members)), superclass(&superclass) {}
+        ClassExprAST(std::string name, std::vector<MemberExprAST> members, ClassExprAST& superclass, std::vector<InterfaceExprAST> implements)
+        : TypenameAST(move(name), move(members)), superclass(&superclass), implements(move(implements)) {}
+    };
+
+    class OutsideImplementsAST : public ExprAST {
+    public:
+        ClassExprAST class_;
+        InterfaceExprAST interface_;
+        OutsideImplementsAST(ClassExprAST& class_, InterfaceExprAST& interface_) : class_(class_), interface_(interface_) {}
+    };
+
+    class VariableExprAST : public ExprAST {
+    public:
+        TypenameAST type;
+        std::string name;
+        VariableExprAST(TypenameAST type, std::string name) : name(move(name)), type(std::move(type)) {}
+    };
+
+    class BinaryExprAST : public ExprAST {
+    public:
+        char op;
+        std::unique_ptr<ExprAST> LHS, RHS;
+
+        BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS,
+                      std::unique_ptr<ExprAST> RHS)
+                : op(op), LHS(move(LHS)), RHS(move(RHS)) {}
+    };
+
+    class ConditionExprAST : public ExprAST {
+    public:
+        std::unique_ptr<ExprAST> condition;
+        std::vector<std::unique_ptr<ExprAST>> initial_branch;
+        std::vector<std::vector<std::unique_ptr<ExprAST>>> else_if_branches;
+        std::vector<std::unique_ptr<ExprAST>> else_branch;
+    };
+
+/// CallExprAST - Expression class for function calls.
+    class CallExprAST : public ExprAST {
+    public:
+        std::string callee;
+        std::vector<std::pair<std::string, ExprAST>> args;
+        CallExprAST(std::string Callee,
+                    std::vector<std::pair<std::string, ExprAST>> args)
+                : callee(move(Callee)), args(move(args)) {}
     };
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -3,8 +3,8 @@
 //
 
 #include "parser.h"
+#include "ast.h"
 
-using enum Token::Kind;
 using std::unique_ptr;
 
 Token ast::Parser::get_next_token() {
@@ -13,79 +13,4 @@ Token ast::Parser::get_next_token() {
 
 Token ast::Parser::get_current_token() {
     return tokens.at(current_index);
-}
-
-std::vector<unique_ptr<ExprAST>> ast::Parser::parse() {
-    std::vector<unique_ptr<ExprAST>>
-    eat();
-    
-    return ast;
-}
-
-unique_ptr<ExprAST> parse_file() {
-    while(!get_current_token().is(Token::Kind::End))) {
-        eat();
-        if(get_current_token().is(Token::Kind::Identifier)) {
-            // parse identifier
-        }
-    }
-}
-
-unique_ptr<ExprAST> parse_number_expr() {
-    auto value = std::make_unique<NumberExprAST>(std::stod(get_current_token().lexeme()));
-    eat();
-    return std::move(value);
-}
-
-unique_ptr<ExprAST> parse_paren_expr() {
-    return unique_ptr<ExprAST>();
-}
-
-unique_ptr<ExprAST> parse_identifier_expr() {
-    Token current = get_current_token();
-    Token next = get_next_token();
-    if(next.is_not(Token::Kind::LeftParen)) {
-        return std::move(std::make_unique<VariableExprAST>(current.lexeme()));
-    }
-    std::vector<unique_ptr<ExprAST>> args;
-    next = get_next_token();
-    if(next.is_not(Token::Kind::RightParen)) { // method has arguments
-        while(1) {
-            auto arg = parse_expr();
-            if(arg) {
-                args.push_back(arg);
-            } else {
-                return nullptr;
-            }
-
-            if(get_current_token().is(Token::Kind::RightParen)) {
-                break;
-            } else if(get_current_token().is_not(Token::Kind::Comma)) {
-                error(get_current_token(), Token::Kind::Comma, Token::Kind::RightParen);
-            }
-            eat();
-        }
-    }
-    eat();
-    return unique_ptr<ExprAST>();
-}
-
-unique_ptr<ExprAST> parse_primary_expr() {
-    return unique_ptr<ExprAST>();
-}
-
-unique_ptr<ExprAST> parse_unary_expr() {
-    return unique_ptr<ExprAST>();
-}
-
-unique_ptr<ExprAST> parse_class_def_expr() {
-    return unique_ptr<ExprAST>();
-}
-
-unique_ptr<ExprAST> parse_outside_inheritance_expr() {
-    return unique_ptr<ExprAST>();
-}
-
-unique_ptr<ExprAST> parse_expr() {
-    return unique_ptr<ExprAST>();
 }
